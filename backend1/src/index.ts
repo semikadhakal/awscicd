1ad702c477d1b25d1243e4ab9b6a4ad2d9c7d36e
semika <semika.vianet.com.np>
2025-07-09 14:16:59 +0545

    feat(filel): filel uplaoded

---
+++ backend1/src/index.ts
@@ -0,0 +1,38 @@
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express, { Application } from 'express';
+
+import appRouter from './routes';
+import { notFound } from './middlewares/notFound';
+import { errorHandler } from './middlewares/errorHandler';
+import {loggerMiddleware, errorLogger} from './middlewares/loggerMiddleware';
+// Clerk imports
+import { ClerkExpressWithAuth } from '@clerk/clerk-sdk-node';
+
+dotenv.config();
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
+app.use(loggerMiddleware);
+
+// Clerk middleware: attaches req.auth to all requests
+app.use(ClerkExpressWithAuth());
+
+app.use(appRouter);
+
+app.use(notFound);
+app.use(errorLogger);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
