e7305ff347c47e646d89ce545e0c9f48811eaf46
semika <semika.vianet.com.np>
2025-07-21 15:14:44 +0545

    feat:(app) finally everything clean

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,8 @@
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 // https://vite.dev/config/
-export default {
-  base : '/trivy-pages/',
-  server: {
-    host: '0.0.0.0', // Bind to all interfaces
-    port: 5173,
-  },
-  
-}
+export default defineConfig({
+  plugins: [react()],
+})
